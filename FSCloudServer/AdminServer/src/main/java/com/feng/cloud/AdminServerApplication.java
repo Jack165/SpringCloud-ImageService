3791c98dbb6f63ebb599649b612191b6edf8e62e
fengshuo <fengshuo>
2021-11-17 21:10:04 +0800

    springbootadmin版本切换到2.3，兼容springboot2.3版本

--- FSCloudServer/AdminServer/src/main/java/com/feng/cloud/AdminServerApplication.java
+++ FSCloudServer/AdminServer/src/main/java/com/feng/cloud/AdminServerApplication.java
@@ -3,14 +3,12 @@ package com.feng.cloud;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author feng
  */
 @SpringBootApplication
 @EnableAdminServer
-@EnableDiscoveryClient
 public class AdminServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServerApplication.class,args);
